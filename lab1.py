662ff0d915531758da27ab470d2cb2280407fa8d
ManuelBentjerodt <mbentjerodt@miuandes.cl>
2021-03-22 15:27:36 -0300

    Creating Cards

--- lab1.py
+++ lab1.py
@@ -1,1 +1,11 @@
-print("Just testing the commit")
+import random
+
+#Generate cards
+noc = int(input("Insert number of cards: ")) #noc = number of cards
+cards = []
+for i in range(1,noc+1):
+    cards.append(i)
+cards *= 2
+random.shuffle(cards)
+
+print(cards)
