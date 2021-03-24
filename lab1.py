000c99195b55a302e6a5e33cf11d6cabcc3e4d96
ManuelBentjerodt <mbentjerodt@miuandes.cl>
2021-03-24 01:14:39 -0300

    ask to user a coordinate

--- lab1.py
+++ lab1.py
@@ -4,16 +4,16 @@
 import random
 
 #Generate cards
-while True: #user have to write a psitive number
+while True: #user have to write a positive number
     try:
         noc = int(input("Insert number of pairs of cards: ")) 
         print()
     except ValueError:
-        print("You must write a number ") 
+        print("You must write a number: ") 
         print()
         continue
     if noc <= 0:
-        print("You must write a positive number ")
+        print("You must write a positive number: ")
         print()
     else:
         break
@@ -24,11 +24,7 @@ for i in range(1,noc+1):
 cards *= 2
 random.shuffle(cards)
 
-hidden_cards = []
-for i in cards:
-    hidden_cards.append("*")
-
-#Generate Board, board as square as possible
+#Generate the Dimensions of the board
 lowest_sum = 4*noc
 
 for i in range(1,2*noc):
@@ -42,37 +38,46 @@ for i in range(1,2*noc):
 print(f"row: {row}, column: {column}")
 
 #the idea of the board of the game, is that the players intectar with the board list type, but in the screen, for esthetic reazon, print the board string type.
-#
-row_board_string = ""  # this will be the rows who will be append in the boards (the type of the row is indicated)
-row_board_list = [] 
-row_board_hidden = ""
 
 board_string = [] #this will be the board (a list that will contain all the rows)
 board_list = [] 
-board_hidden = []
-
+board_hidden_string = []
+board_hidden_list = []
 
 for i in range(row):
-    row_board_string = ""
+    row_board_string = "" #this will be the rows who will be appended in the board list type
     row_board_list = []
-    row_board_hidden= ""
+    row_board_hidden_string = ""
+    row_board_hidden_list = []
+
     for j in range(column):
-        
         row_board_string += str(" ")*(len(str(noc))-len(str(cards[-1]))) +str(cards[-1])+str(" ")
         row_board_list.append(cards[-1])
-        row_board_hidden +=  str("*")*len(str(noc))+str(" ")
+        row_board_hidden_string +=  str("*")*len(str(noc))+str(" ")
+        row_board_hidden_list.append(str("*")*len(str(noc))+str(" "))
 
         cards.pop()
+
     board_string.append(row_board_string)
     board_list.append(row_board_list)
-    board_hidden.append(row_board_hidden)
-    
+    board_hidden_string.append(row_board_hidden_string)
+    board_hidden_list.append(row_board_hidden_list)
 
-for i in board_string:
-    print(i)
+def ask_for_coordinate():
+    cord = input("Enter a tuple 'a,b': ")
+    cord = cord.split(",")
+    a = int(cord[0])
+    b = int(cord[1])
 
-for i in board_hidden:
-    print(i)
+    if a > row or a <= 0 or b > column or b <= 0:
+        print("Invalid coordinate, make sure both numbers are positive and equal to or less than the board dimension")
+        ask_for_coordinate()
+    else:
+        return (a,b)
+
+print(ask_for_coordinate())
+
+
 
 
 
