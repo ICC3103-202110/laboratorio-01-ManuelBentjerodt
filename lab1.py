d9d3785a6d42cd144f4b9f5d5daa6f8db2e2a173
ManuelBentjerodt <mbentjerodt@miuandes.cl>
2021-03-25 04:00:45 -0300

    Almost ready, need to define winner

--- lab1.py
+++ lab1.py
@@ -27,7 +27,12 @@ for i in range(1,noc+1):
 cards *= 2
 random.shuffle(cards)
 
-repeated = [] #this variable counts the number of the moves in a turn and something more
+repeated_cord = [] #this list appends the coordinates in move (2 times player flipping a card)
+repeated_val = [] #this list appends the value of the coordinate
+correct_fliped = [] #this list appends the card they cant be played again
+
+
+points = [0,0] #points[0] are the points of player 1, and points[1] are the points of player 2
 
 
 #Generate the Dimensions of the board
@@ -81,12 +86,11 @@ def ask_for_coordinate(): #thi function asks to user the coordinate of the board
     cord_ = cord
     cord = cord.split(",")
     
-    
     a = int(cord[0])
     b = int(cord[1])
 
-    if cord_ in repeated:
-        print("Coordinite in use")
+    if cord_ in repeated_cord or cord_ in correct_fliped:
+        print("Coordinite in use or unable")
         return ask_for_coordinate()
 
     else:
@@ -96,7 +100,7 @@ def ask_for_coordinate(): #thi function asks to user the coordinate of the board
             return ask_for_coordinate()
 
         else:
-            repeated.append(cord_)
+            repeated_cord.append(cord_)
             return [a,b]
      
 
@@ -104,18 +108,18 @@ def flip_card_and_show(l): #l parameter will be the return of ask_for_coordinate
     print("____________________________________________________________")
     card = board[l[0]-1][l[1]-1]
     board_hidden[l[0]-1][l[1]-1] = str(" ")*(len(str(noc))-len(str(card))) +str(card) +str(" ")
-    round_cards.append(card)
+    repeated_val.append(card)
 
     for i in transform(board_hidden):
         print(i)
 
-round_cards = []
-
 def player_move(n):
     print("____________________________________________________________")
-    print(f"Player {n}")
+    print(f"Player {n}    Points: {points[n-1]}")
     print()
     print(f"There are {row} ROWS and {column} Columnes")
+
+    n=n
 
     for i in transform(board_hidden):
         print(i)
@@ -123,14 +127,44 @@ def player_move(n):
     flip_card_and_show(ask_for_coordinate())
     flip_card_and_show(ask_for_coordinate()) 
 
-def main():
-    player_1 = 0 #points
-    player_2 = 0
+    a = int(repeated_cord[0].split(",")[0])
+    b = int(repeated_cord[0].split(",")[1])
+    
+    c = int(repeated_cord[1].split(",")[0])
+    d = int(repeated_cord[1].split(",")[1]) 
 
-    l = [1,2]
+    for i in range(len(repeated_cord)):
+        repeated_cord.pop()
+        repeated_val.pop()
+
+    if board[a-1][b-1] == board[c-1][d-1]:
+        correct_fliped.append(str(a)+str(",")+str(b))
+        correct_fliped.append(str(c)+str(",")+str(d))
+
+        board_hidden[a-1][b-1] = str(" ")*(len(str(noc))) + str(" ")
+        board_hidden[c-1][d-1] = str(" ")*(len(str(noc))) + str(" ")
+        
+        points[n-1] += 1
+        return player_move(n)
+   
+    elif board[a-1][b-1] != board[c-1][d-1]:
+        board_hidden[a-1][b-1] = str("*")*(len(str(noc))) + str(" ")
+        board_hidden[c-1][d-1] = str("*")*(len(str(noc))) + str(" ")
+    
+
+def main():
+    print()
+    print("The board filpped is:")
+    print()
+    for i in transform(board):
+        print(i)
+
+    print()
+
+    l = [1,2]*noc**noc
     for i in l:
         player_move(i)
-        print(round_cards)
+        print(correct_fliped)
         
 main()
  
