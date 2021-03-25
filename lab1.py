237c6936c525efc8b9b136545793d420afb06448
ManuelBentjerodt <mbentjerodt@miuandes.cl>
2021-03-25 04:25:01 -0300

    Comments added

--- lab1.py
+++ lab1.py
@@ -27,9 +27,9 @@ for i in range(1,noc+1):
 cards *= 2
 random.shuffle(cards)
 
-repeated_cord = [] #this list appends the coordinates in move (2 times player flipping a card)
-repeated_val = [] #this list appends the value of the coordinate
-correct_fliped = [] #this list appends the card they cant be played again
+repeated_cord = [] #this list appends the coordinates in move (2 times player flipping a card) It's temporary
+repeated_val = [] #this list appends the value of the coordinate. It's temporary
+correct_fliped = [] #this list appends the card they cant be played again. It's permanent
 
 
 points = [0,0] #points[0] are the points of player 1, and points[1] are the points of player 2
@@ -48,7 +48,6 @@ for i in range(1,2*noc):
 
 #Generate the board with cards, and another with the card hidden 
 #the idea of the board of the game, is that the players intectar with the board list type, but in the screen, for esthetic reazon, print the board string type.
-
 board = [] 
 board_hidden = []
 
@@ -89,8 +88,8 @@ def ask_for_coordinate(): #thi function asks to user the coordinate of the board
     a = int(cord[0])
     b = int(cord[1])
 
-    if cord_ in repeated_cord or cord_ in correct_fliped:
-        print("Coordinite in use or unable")
+    if cord_ in repeated_cord or cord_ in correct_fliped:  #these are the conditions that makes player cant insert a coordinate actually used,
+        print("Coordinite in use or unable")               #a card actually flipped succefuy or a coordinate out of the board
         return ask_for_coordinate()
 
     else:
@@ -104,7 +103,7 @@ def ask_for_coordinate(): #thi function asks to user the coordinate of the board
             return [a,b]
      
 
-def flip_card_and_show(l): #l parameter will be the return of ask_for_coordinate function
+def flip_card_and_show(l): #function flip a card and show the board actualiced, l parameter will be the return of ask_for_coordinate function
     print("____________________________________________________________")
     card = board[l[0]-1][l[1]-1]
     board_hidden[l[0]-1][l[1]-1] = str(" ")*(len(str(noc))-len(str(card))) +str(card) +str(" ")
@@ -113,7 +112,7 @@ def flip_card_and_show(l): #l parameter will be the return of ask_for_coordinate
     for i in transform(board_hidden):
         print(i)
 
-def player_move(n):
+def player_move(n):  #This functions makes player have to flipp card twice, 
     print("____________________________________________________________")
     print(f"Player {n}    Points: {points[n-1]}")
     print()
@@ -124,20 +123,20 @@ def player_move(n):
     for i in transform(board_hidden):
         print(i)
 
-    flip_card_and_show(ask_for_coordinate())
+    flip_card_and_show(ask_for_coordinate())  #here player flipp card twice
     flip_card_and_show(ask_for_coordinate()) 
 
-    a = int(repeated_cord[0].split(",")[0])
-    b = int(repeated_cord[0].split(",")[1])
+    a = int(repeated_cord[0].split(",")[0]) #this variables saves the the two coordinates user have to insert
+    b = int(repeated_cord[0].split(",")[1]) 
     
     c = int(repeated_cord[1].split(",")[0])
     d = int(repeated_cord[1].split(",")[1]) 
 
-    for i in range(len(repeated_cord)):
+    for i in range(len(repeated_cord)): #delete the history of cards played in a turn
         repeated_cord.pop()
         repeated_val.pop()
 
-    if board[a-1][b-1] == board[c-1][d-1]:
+    if board[a-1][b-1] == board[c-1][d-1]: #here, if user flipp the 2 equals cards, remove those cards of the board and play again
         correct_fliped.append(str(a)+str(",")+str(b))
         correct_fliped.append(str(c)+str(",")+str(d))
 
@@ -147,24 +146,25 @@ def player_move(n):
         points[n-1] += 1
         return player_move(n)
    
-    elif board[a-1][b-1] != board[c-1][d-1]:
+    elif board[a-1][b-1] != board[c-1][d-1]: #in the other hand, if user flipp 2 differents cards, return again those cards in the board (hidden)
         board_hidden[a-1][b-1] = str("*")*(len(str(noc))) + str(" ")
         board_hidden[c-1][d-1] = str("*")*(len(str(noc))) + str(" ")
     
 
-def main():
-    print()
-    print("The board filpped is:")
+def main(): #this function is the base of the game, switch between players and call other fuctions.
+    #this hidden part of the code shows the board with all the cards flipped,
+    """
+    print()                                 
+    print("The board filpped is:") 
     print()
     for i in transform(board):
         print(i)
 
     print()
-
+    """ 
     l = [1,2]*noc**noc
     for i in l:
         player_move(i)
-        print(correct_fliped)
         
-main()
+main() #start the game
  
